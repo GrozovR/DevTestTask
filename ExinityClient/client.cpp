ea084808d05fa14b268d7f06042d5a5d3e0f13c3
Roman Grozov <rmangrzv@gmail.com>
2025-02-24 10:21:22 +0200

    client final

--- ExinityClient/client.cpp
+++ ExinityClient/client.cpp
@@ -1,8 +1,5 @@
 #include "client.h"
 
-// TODO:!
-#include <iostream>
-
 namespace exinity {
 
 using namespace boost::asio;
@@ -44,18 +41,14 @@ void client::connect(const std::string& host, const std::string& port)
 
 void client::close()
 {
-    // TODO:
-
-    //logFile.close();
     boost::system::error_code ec;
     socket.shutdown(ip::tcp::socket::shutdown_both, ec);
     if (ec) {
-        // Not all sockets support shutdown (e.g., already closed or not connected).
-        // // You can log or handle the error, but it's often fine to ignore.
+        logger_.logError(ec.message());
     }
     socket.close(ec);
     if (ec) {
-        // Handle or log if the close failed (rare).
+        logger_.logError(ec.message());
     }
 }
 
@@ -67,10 +60,10 @@ void client::doConnect(const ip::tcp::resolver::results_type& endpoints)
         endpoints,
         [this, self](const boost::system::error_code& ec, const ip::tcp::endpoint& /*endpoint*/) {
             if (!ec) {
-                log("Connected to server.");
+                logger_.log("Connected to server.");
                 sendRandomNumber();
             } else {
-                log( "Connect failed: " + ec.message() );
+                logger_.logError( "Connect failed: " + ec.message() );
                 // TODO: можно сделать несколько попыток
             }
         }
@@ -80,7 +73,7 @@ void client::doConnect(const ip::tcp::resolver::results_type& endpoints)
 void client::doRead()
 {
     if (!socket.is_open()) {
-        std::cerr << "Socket is not open!" << std::endl;
+        logger_.logError("Socket is not open!");
         return;
     }
 
@@ -89,29 +82,27 @@ void client::doRead()
         socket,
         streamBuffer,
         '\n',
-        [this, self](const boost::system::error_code& ec, std::size_t /*bytes_transferred*/) {
+        [this, self](const boost::system::error_code& ec, std::size_t) {
             if (!ec) {
                 std::istream is(&streamBuffer);
                 std::string line;
                 std::getline(is, line);
-                log("Received from server: " + line);
+                logger_.log("Received from server: " + line);
 
                 sendRandomNumber();
 
             } else {
-                std::cerr << "[Client] Read error: " << ec.message() << std::endl;
-                // TODO: может быть переотрыть сокет ?
-                socket.close();
+                logger_.logError("Reading error: " + ec.message());
+                close();
             }
         }
     );
 }
 
-
 void client::sendRandomNumber()
 {
     if (!socket.is_open()) {
-        std::cerr << "Socket is not open!" << std::endl;
+        logger_.logError("Socket is not open!");
         return;
     }
 
@@ -119,27 +110,19 @@ void client::sendRandomNumber()
     short number = generateNextNumber();
     current_message = std::to_string(number) + "\n";
 
-    log("Sending: " + current_message);
+    logger_.log("Sending: " + current_message);
 
     auto self(shared_from_this());
     async_write(socket, buffer(current_message),
-        [this, self](const boost::system::error_code& ec, std::size_t /*bytes_transferred*/) {
+        [this, self](const boost::system::error_code& ec, std::size_t) {
             if (!ec) {
                 doRead();
             } else {
-                std::cerr << "Send error: " << ec.message() << std::endl;
-                // TODO: может быть переотрыть сокет ?
-                socket.close();
+                logger_.logError( "Sending error: " + ec.message());
+                close();
             }
         }
     );
-}
-
-void client::log(std::string_view message)
-{
-    std::stringstream messageStr;
-    messageStr << std::chrono::system_clock::now() << " " << message;
-    logger_.log(messageStr.str());
 }
 
 } // namespace exinity
